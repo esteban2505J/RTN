061f964f181859a9fd10be2442fb9904723939dd
esteban2505J <juane.ramirezt@uvirtual.edu.co>
2023-10-24 16:45:02 -0500

    changes responsive navBar

--- RTN/src/components/shared/Sidebar.jsx
+++ RTN/src/components/shared/Sidebar.jsx
@@ -9,11 +9,11 @@ export const Sidebar = () => {
   return (
     <>
       <div
-        className={` bg-[#8cb7bc] fixed bottom-0 md:left-0 items-center  md:w-28 md:h-full w-full  flex h-20 md:flex-col flex-row justify-between py-6 rounded-tr-xl rounded-br-xl z-50  mb-0 left-0 `}
+        className={` bg-[#8cb7bc] fixed bottom-0 md:left-0 items-center md:w-28 md:h-full w-full  flex h-20 md:flex-col flex-row justify-between py-6 rounded-tr-xl rounded-br-xl z-50  mb-0 left-0 `}
       >
         <div className="">
           <ul className="pl-4 flex ">
-            <li className="hover:bg-white p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
+            <li className="hover:bg-white p-1 md:p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
               <Link
                 to="/"
                 className="group-hover:bg-[#262837]  group-hover:text-white p-3 flex justify-center rounded-xl text-salte-700 transition-colors"
@@ -22,7 +22,7 @@ export const Sidebar = () => {
               </Link>
             </li>
 
-            <li className=" hover:bg-white p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
+            <li className=" hover:bg-white p-1 md:p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
               <Link
                 to="/"
                 className="group-hover:bg-[#262837]  group-hover:text-white p-3 flex justify-center rounded-xl text-salte-700 transition-colors"
@@ -31,7 +31,7 @@ export const Sidebar = () => {
               </Link>
             </li>
 
-            <li className=" hover:bg-white p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
+            <li className=" hover:bg-white p-1 md:p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
               <Link
                 to="/"
                 className="group-hover:bg-[#262837]  group-hover:text-white p-3 flex justify-center rounded-xl text-salte-700 transition-colors"
@@ -40,7 +40,7 @@ export const Sidebar = () => {
               </Link>
             </li>
 
-            <li className=" hover:bg-white p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
+            <li className=" hover:bg-white p-1 md:p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
               <Link
                 to="/"
                 className="group-hover:bg-[#262837]  group-hover:text-white p-3 flex justify-center rounded-xl text-salte-700 transition-colors"
@@ -49,7 +49,7 @@ export const Sidebar = () => {
               </Link>
             </li>
 
-            <li className=" hover:bg-white p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
+            <li className=" hover:bg-white p-1 md:p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
               <Link
                 to="/"
                 className="group-hover:bg-[#262837]  group-hover:text-white p-3 flex justify-center rounded-xl text-salte-700 transition-colors"
@@ -58,7 +58,7 @@ export const Sidebar = () => {
               </Link>
             </li>
 
-            <li className=" hover:bg-whiteS p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
+            <li className=" hover:bg-whiteS p-1 md:p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
               <Link
                 to="/"
                 className="group-hover:bg-[#262837]  group-hover:text-white p-3 flex justify-center rounded-xl text-salte-700 transition-colors"
@@ -71,7 +71,7 @@ export const Sidebar = () => {
 
         <div className="pl-4">
           <ul>
-            <li className=" hover:bg-white p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
+            <li className=" hover:bg-white p-1 md:p-3 rounded-tl-lg rounded-bl-lg group transition-colors">
               <Link
                 to="/"
                 className="group-hover:bg-[#262837]  group-hover:text-white p-3 flex justify-center rounded-xl text-salte-700 transition-colors"
